e976ec4c1eab03853ccd7425fcf67273f6f3e9a0
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-08-17 17:44:39 -0300

    REFACTOR: add bean validation in Entity classes with jakarta validation

--- src/main/java/ar/com/l_airline/domain/entities/Room.java
+++ src/main/java/ar/com/l_airline/domain/entities/Room.java
@@ -4,6 +4,9 @@ import ar.com.l_airline.domain.enums.BedsType;
 import ar.com.l_airline.domain.enums.RoomState;
 import ar.com.l_airline.domain.enums.RoomType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;
@@ -38,21 +41,36 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Min(1)
     private int floor;
+    @NotNull
+    @Min(1)
+    @Max(4)
     private int numberOfBeds;
+    @NotNull
     @Enumerated(EnumType.STRING)
     private BedsType bedType;
+    @NotNull
+    @Min(1)
+    @Max(4)
     private int peopleCapacity;
     @Enumerated(EnumType.STRING)
+    @NotNull
     private RoomType roomType;
     @Enumerated(EnumType.STRING)
+    @NotNull
     private RoomState state;
+    @NotNull
     private int timeWasBooked;
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "hotel_id", referencedColumnName = "id")
     private Hotel hotel;
+    @NotNull
     @OneToMany(mappedBy = "roomBooked")
     private List<Reservation> reservation;
+    @NotNull
     @OneToMany(mappedBy = "room")
     private List<RoomBookingPeriod> roomBookingPeriod;
 
