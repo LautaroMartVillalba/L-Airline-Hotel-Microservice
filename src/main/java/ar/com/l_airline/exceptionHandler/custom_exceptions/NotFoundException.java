77b979c70ee92d53c805d0b5a39d07cb53aeebc9
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-08-23 22:53:39 -0300

    DELETE: redundant and unused custom exceptions

--- src/main/java/ar/com/l_airline/exceptionHandler/custom_exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ar.com.l_airline.exceptionHandler.custom_exceptions;
-
-/**
- * This exception should be thrown when there are no matches in the database for the received data.
- */
-public class NotFoundException extends RuntimeException {
-}
